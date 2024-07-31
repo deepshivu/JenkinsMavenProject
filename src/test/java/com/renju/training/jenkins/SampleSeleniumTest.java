080a30438330e65b27bc3435f1e40560c2216f65
deepshivu <162087553+deepshivu@users.noreply.github.com>
2024-07-31 13:34:58 +0530

    Update SampleSeleniumTest.java

--- src/test/java/com/renju/training/jenkins/SampleSeleniumTest.java
+++ src/test/java/com/renju/training/jenkins/SampleSeleniumTest.java
@@ -1,45 +1,58 @@
-package com.renju.training.jenkins;
+package com.deepika.jenkins4;
 
+import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.AssertJUnit;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class SampleSeleniumTest {
-	
-	WebDriver driver;
-	
-	@BeforeClass
-	public void startBrowser() {
-		System.setProperty("webdriver.chrome.driver", "F:\\Downloads\\chromedriver_win32\\chromedriver.exe");
-		ChromeOptions chromeOptions = new ChromeOptions();
-		chromeOptions.addArguments("--start-maximized");
-		driver = new ChromeDriver(chromeOptions);
-	}
-	
-	@Test
-	public void validateGoogleId() throws Exception {
-		System.out.println("Opening Browser");
-		driver.get("http://www.google.com");
-		System.out.println("Clicking Gmail Link");
-		driver.findElement(By.xpath("//*[@id=\"gbw\"]/div/div/div[1]/div[1]/a")).click();
-		System.out.println("Clicking Sign In link");
-		driver.findElement(By.xpath("/html/body/nav/div/a[2]")).click();
-		System.out.println("Entering username");
-		driver.findElement(By.xpath("//*[@id=\"identifierId\"]")).sendKeys("renju.jenkins.training");
-		System.out.println("Clicking Next button");
-		driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/content/span")).click();
-		Thread.sleep(5000);
-		boolean textFound = driver.getPageSource().contains("Forgot password");
-		AssertJUnit.assertTrue(textFound);
-	}
-	
-	@AfterClass
-	public void closeBrowser() {
-		driver.quit();
-	}
+    
+    WebDriver driver;
+    WebDriverWait wait;
+    
+    @BeforeClass
+    public void startBrowser() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\ADMIN\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--start-maximized");
+        driver = new ChromeDriver(chromeOptions);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Initialize WebDriverWait with 10 seconds timeout
+    }
+    
+    @Test
+    public void validateGoogleId() throws Exception {
+        System.out.println("Opening Browser");
+        driver.get("http://www.google.com");
+
+        System.out.println("Clicking Gmail Link");
+        WebElement gmailLink = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Gmail")));
+        gmailLink.click();
+
+        System.out.println("Clicking Sign In link");
+        WebElement signInLink = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Sign in")));
+        signInLink.click();
+
+        System.out.println("Entering username");
+        WebElement usernameField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("identifierId")));
+        usernameField.sendKeys("deepika9785");
+
+        System.out.println("Clicking Next button");
+        WebElement nextButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='identifierNext']/div/button/span")));
+        nextButton.click();
+
+     
+    }
+    
+    @AfterClass
+    public void closeBrowser() {
+        driver.quit();
+    }
 }
